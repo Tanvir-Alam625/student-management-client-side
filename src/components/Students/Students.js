65e9ccfa56909e9e4300836a2e860178d86d4a7b
tanveer <mdtanviralamht731@gmail.com>
2022-06-07 18:14:09 +0600

    student component design all done

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -2,17 +2,37 @@ import React from "react";
 
 const Students = () => {
   return (
-    <div className="font-sans max-w-[1100px] mx-auto">
+    <div className="font-sans max-w-[1100px] mx-auto px-2">
       <div className="stu-header py-4">
-        <h2 className="text-4xl text-center text-teal-600 font-semibold ">
+        <h2 className="text-4xl text-center text-primary font-semibold ">
           Student Management
         </h2>
-        <h5 className="text-center text-2xl  text-teal-500">
+        <h5 className="text-center text-2xl  text-primary">
           Barguna Polytechnic Institute
         </h5>
       </div>
+      <div className="search-container my-8 lg:w-2/5 md:w-2/3 w-full  mx-auto">
+        <h2 className="text-xl my-3 text-primary  font-semibold text-center">
+          Search Student
+        </h2>
+        <form className="flex justify-center">
+          <input
+            type="number"
+            required
+            className=" border-2 border-primary px-4 py-2 rounded-l-md  shadow-md w-full max-w-xs bg-transparent outline-0"
+            placeholder="Type Student Roll"
+          />
+          <button className="py-2 px-4 bg-primary text-white rounded-r-md font-semibold ">
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="flex justify-between mt-6  mb-2">
+        <button className="btn btn-primary">See All Students</button>
+        <button className="btn btn-primary"> Add New Student</button>
+      </div>
       <div class="overflow-x-auto w-full">
-        <table class="table w-full">
+        <table class="table w-full bg-transparent">
           {/* <!-- head --> */}
           <thead>
             <tr>
